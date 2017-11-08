2f9242ca7731e19a1376d1e208182669b8d753c8
frangv1991 <frangv1991@gmail.com>
2017-11-08 20:38:59 +0100

    Commit clase miércoles 08/11/2017 ApiRest.

--- src/main/java/api/resources/PlaylistResource.java
+++ src/main/java/api/resources/PlaylistResource.java
@@ -25,150 +25,151 @@ import model.Song;
 import model.repository.MapPlaylistRepository;
 import model.repository.PlaylistRepository;
 
-
 @Path("/lists")
 public class PlaylistResource {
-	
-	/* Singleton */
-	private static PlaylistResource _instance=null;
-	PlaylistRepository repository;
-	
-	private PlaylistResource() {
-		repository=MapPlaylistRepository.getInstance();
 
-	}
-	
-	public static PlaylistResource getInstance()
-	{
-		if(_instance==null)
-				_instance=new PlaylistResource();
-		return _instance;
-	}
-	
+    /* Singleton */
+    private static PlaylistResource _instance = null;
+    PlaylistRepository repository;
 
-	@GET
-	@Produces("application/json")
-	public Collection<Playlist> getAll()
-	{
-		return repository.getAllPlaylists();
-	}
-	
-	
-	@GET
-	@Path("/{id}")
-	@Produces("application/json")
-	public Playlist get(@PathParam("id") String id)
-	{
-		Playlist list = repository.getPlaylist(id);
-		
-		if (list == null) {
-			throw new NotFoundException("The playlist wit id="+ id +" was not found");			
-		}
-		
-		return list;
-	}
-	
-	@POST
-	@Consumes("application/json")
-	@Produces("application/json")
-	public Response addPlaylist(@Context UriInfo uriInfo, Playlist playlist) {
-		if (playlist.getName() == null || "".equals(playlist.getName()))
-			throw new BadRequestException("The name of the playlist must not be null");
-		
-		if (playlist.getSongs()!=null)
-			throw new BadRequestException("The songs property is not editable.");
+    private PlaylistResource() {
+        repository = MapPlaylistRepository.getInstance();
 
-		repository.addPlaylist(playlist);
+    }
 
-		// Builds the response. Returns the playlist the has just been added.
-		UriBuilder ub = uriInfo.getAbsolutePathBuilder().path(this.getClass(), "get");
-		URI uri = ub.build(playlist.getId());
-		ResponseBuilder resp = Response.created(uri);
-		resp.entity(playlist);			
-		return resp.build();
-	}
+    public static PlaylistResource getInstance() {
+        if (_instance == null) {
+            _instance = new PlaylistResource();
+        }
+        return _instance;
+    }
 
-	
-	@PUT
-	@Consumes("application/json")
-	public Response updatePlaylist(Playlist playlist) {
-		Playlist oldplaylist = repository.getPlaylist(playlist.getId());
-		if (oldplaylist == null) {
-			throw new NotFoundException("The playlist with id="+ playlist.getId() +" was not found");			
-		}
-		
-		if (playlist.getSongs()!=null)
-			throw new BadRequestException("The songs property is not editable.");
-		
-		// Update name
-		if (playlist.getName()!=null)
-			oldplaylist.setName(playlist.getName());
-		
-		// Update description
-		if (playlist.getDescription()!=null)
-			oldplaylist.setDescription(playlist.getDescription());
-		
-		return Response.noContent().build();
-	}
-	
-	@DELETE
-	@Path("/{id}")
-	public Response removePlaylist(@PathParam("id") String id) {
-		Playlist toberemoved=repository.getPlaylist(id);
-		if (toberemoved == null)
-			throw new NotFoundException("The playlist with id="+ id +" was not found");
-		else
-			repository.deletePlaylist(id);
-		
-		return Response.noContent().build();
-	}
-	
-	
-	@POST	
-	@Path("/{playlistId}/{songId}")
-	@Consumes("text/plain")
-	@Produces("application/json")
-	public Response addSong(@Context UriInfo uriInfo,@PathParam("playlistId") String playlistId, @PathParam("songId") String songId)
-	{				
-		
-		Playlist playlist = repository.getPlaylist(playlistId);
-		Song song = repository.getSong(songId);
-		
-		if (playlist==null)
-			throw new NotFoundException("The playlist with id=" + playlistId + " was not found");
-		
-		if (song == null)
-			throw new NotFoundException("The song with id=" + songId + " was not found");
-		
-		if (playlist.getSong(songId)!=null)
-			throw new BadRequestException("The song is already included in the playlist.");
-			
-		repository.addSong(playlistId, songId);		
+    @GET
+    @Produces("application/json")
+    public Collection<Playlist> getAll() {
+        return repository.getAllPlaylists();
+    }
 
-		// Builds the response
-		UriBuilder ub = uriInfo.getAbsolutePathBuilder().path(this.getClass(), "get");
-		URI uri = ub.build(playlistId);
-		ResponseBuilder resp = Response.created(uri);
-		resp.entity(playlist);			
-		return resp.build();
-	}
-	
-	
-	@DELETE
-	@Path("/{playlistId}/{songId}")
-	public Response removeSong(@PathParam("playlistId") String playlistId, @PathParam("songId") String songId) {
-		Playlist playlist = repository.getPlaylist(playlistId);
-		Song song = repository.getSong(songId);
-		
-		if (playlist==null)
-			throw new NotFoundException("The playlist with id=" + playlistId + " was not found");
-		
-		if (song == null)
-			throw new NotFoundException("The song with id=" + songId + " was not found");
-		
-		
-		repository.removeSong(playlistId, songId);		
-		
-		return Response.noContent().build();
-	}
+    @GET
+    @Path("/{id}")
+    @Produces("application/json")
+    public Playlist get(@PathParam("id") String id) {
+        Playlist list = repository.getPlaylist(id);
+
+        if (list == null) {
+            throw new NotFoundException("The playlist with id=" + id + " was not found");
+        }
+
+        return list;
+    }
+
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response addPlaylist(@Context UriInfo uriInfo, Playlist playlist) {
+        if (playlist.getName() == null || "".equals(playlist.getName())) {
+            throw new BadRequestException("The name of the playlist must not be null");
+        }
+
+        if (playlist.getSongs() != null) {
+            throw new BadRequestException("The songs property is not editable.");
+        }
+
+        repository.addPlaylist(playlist);
+
+        // Builds the response. Returns the playlist the has just been added.
+        UriBuilder ub = uriInfo.getAbsolutePathBuilder().path(this.getClass(), "get");
+        URI uri = ub.build(playlist.getId());
+        ResponseBuilder resp = Response.created(uri);
+        resp.entity(playlist);
+        return resp.build();
+    }
+
+    @PUT
+    @Consumes("application/json")
+    public Response updatePlaylist(Playlist playlist) {
+        Playlist oldplaylist = repository.getPlaylist(playlist.getId());
+        if (oldplaylist == null) {
+            throw new NotFoundException("The playlist with id=" + playlist.getId() + " was not found");
+        }
+
+        if (playlist.getSongs() != null) {
+            throw new BadRequestException("The songs property is not editable.");
+        }
+
+        // Update name
+        if (playlist.getName() != null) {
+            oldplaylist.setName(playlist.getName());
+        }
+
+        // Update description
+        if (playlist.getDescription() != null) {
+            oldplaylist.setDescription(playlist.getDescription());
+        }
+
+        return Response.noContent().build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response removePlaylist(@PathParam("id") String id) {
+        Playlist toberemoved = repository.getPlaylist(id);
+        if (toberemoved == null) {
+            throw new NotFoundException("The playlist with id=" + id + " was not found");
+        } else {
+            repository.deletePlaylist(id);
+        }
+
+        return Response.noContent().build();
+    }
+
+    @POST
+    @Path("/{playlistId}/{songId}")
+    @Consumes("text/plain")
+    @Produces("application/json")
+    public Response addSong(@Context UriInfo uriInfo, @PathParam("playlistId") String playlistId, @PathParam("songId") String songId) {
+
+        Playlist playlist = repository.getPlaylist(playlistId);
+        Song song = repository.getSong(songId);
+
+        if (playlist == null) {
+            throw new NotFoundException("The playlist with id=" + playlistId + " was not found");
+        }
+
+        if (song == null) {
+            throw new NotFoundException("The song with id=" + songId + " was not found");
+        }
+
+        if (playlist.getSong(songId) != null) {
+            throw new BadRequestException("The song is already included in the playlist.");
+        }
+
+        repository.addSong(playlistId, songId);
+
+        // Builds the response
+        UriBuilder ub = uriInfo.getAbsolutePathBuilder().path(this.getClass(), "get");
+        URI uri = ub.build(playlistId);
+        ResponseBuilder resp = Response.created(uri);
+        resp.entity(playlist);
+        return resp.build();
+    }
+
+    @DELETE
+    @Path("/{playlistId}/{songId}")
+    public Response removeSong(@PathParam("playlistId") String playlistId, @PathParam("songId") String songId) {
+        Playlist playlist = repository.getPlaylist(playlistId);
+        Song song = repository.getSong(songId);
+
+        if (playlist == null) {
+            throw new NotFoundException("The playlist with id=" + playlistId + " was not found");
+        }
+
+        if (song == null) {
+            throw new NotFoundException("The song with id=" + songId + " was not found");
+        }
+
+        repository.removeSong(playlistId, songId);
+
+        return Response.noContent().build();
+    }
 }
--- src/main/java/api/resources/SongResource.java
+++ src/main/java/api/resources/SongResource.java
@@ -26,59 +26,117 @@ import java.net.URI;
 import java.util.Collection;
 import java.util.List;
 
-
 @Path("/songs")
 public class SongResource {
 
-	public static SongResource _instance=null;
-	PlaylistRepository repository;
-	
-	private SongResource(){
-		repository=MapPlaylistRepository.getInstance();
-	}
-	
-	public static SongResource getInstance()
-	{
-		if(_instance==null)
-			_instance=new SongResource();
-		return _instance; 
-	}
-	
-	@GET
-	@Produces("application/json")
-	public Collection<Song> getAll()
-	{
-		return null;
-	}
-	
-	
-	@GET
-	@Path("/{id}")
-	@Produces("application/json")
-	public Song get(@PathParam("id") String songId)
-	{
-		
-		return null;
-	}
-	
-	@POST
-	@Consumes("application/json")
-	@Produces("application/json")
-	public Response addSong(@Context UriInfo uriInfo, Song song) {
-		return null;
-	}
-	
-	
-	@PUT
-	@Consumes("application/json")
-	public Response updateSong(Song song) {
-		return null;
-	}
-	
-	@DELETE
-	@Path("/{id}")
-	public Response removeSong(@PathParam("id") String songId) {
-		return null;
-	}
-	
+    public static SongResource _instance = null;
+    PlaylistRepository repository;
+
+    private SongResource() {
+        repository = MapPlaylistRepository.getInstance();
+    }
+
+    public static SongResource getInstance() {
+        if (_instance == null) {
+            _instance = new SongResource();
+        }
+        return _instance;
+    }
+
+    @GET
+    @Produces("application/json")
+    public Collection<Song> getAll() {
+        return repository.getAllSongs();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces("application/json")
+    public Song get(@PathParam("id") String songId) {
+        Song song = repository.getSong(songId);
+
+        if (song == null) {
+            throw new NotFoundException("The song with id=" + songId + " was not found");
+        }
+
+        return song;
+    }
+
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response addSong(@Context UriInfo uriInfo, Song song) {
+        if (song.getTitle() == null || "".equals(song.getTitle())) {
+            throw new BadRequestException("The name of the song must not be null");
+        }
+        
+        if (song.getArtist()== null || "".equals(song.getArtist())) {
+            throw new BadRequestException("The artist of the song must not be null");
+        }
+        
+        if (song.getAlbum()== null || "".equals(song.getAlbum())) {
+            throw new BadRequestException("The album of the song must not be null");
+        }
+        
+        if (song.getYear()== null || "".equals(song.getYear())) {
+            throw new BadRequestException("The year of the song must not be null");
+        }
+
+        repository.addSong(song);
+
+        // Builds the response. Returns the song the has just been added.
+        UriBuilder ub = uriInfo.getAbsolutePathBuilder().path(this.getClass(), "get");
+        URI uri = ub.build(song.getId());
+        ResponseBuilder resp = Response.created(uri);
+        resp.entity(song);
+        
+        return resp.build();
+    }
+
+    @PUT
+    @Consumes("application/json")
+    public Response updateSong(Song song) {
+        Song oldSong = repository.getSong(song.getId());
+        
+        if (oldSong == null) {
+            throw new NotFoundException("The song with id=" + song.getId() + " was not found");
+        }
+
+        // Update title
+        if (song.getTitle() != null) {
+            oldSong.setTitle(song.getTitle());
+        }
+
+        // Update artist
+        if (song.getArtist()!= null) {
+            oldSong.setArtist(song.getArtist());
+        }
+        
+        // Update album
+        if (song.getAlbum()!= null) {
+            oldSong.setAlbum(song.getAlbum());
+        }
+        
+        // Update year
+        if (song.getYear()!= null) {
+            oldSong.setYear(song.getYear());
+        }
+
+        return Response.noContent().build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response removeSong(@PathParam("id") String songId) {
+        Song toberemoved = repository.getSong(songId);
+        
+        if (toberemoved == null) {
+            throw new NotFoundException("The song with id=" + songId + " was not found");
+        } else {
+            repository.deleteSong(songId);
+        }
+
+        return Response.noContent().build();
+    }
+
 }
